562e1ff275acb8e982c93087512d0175f8aadaf8
ansukiasyan <an.sukiasyan@gmail.com>
2020-04-24 02:21:52 +0400

    Create a bucket containing an index.html with public read access

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "html" {
+  bucket        = "annas-twink-s3"
+  force_destroy = true
+  acl           = "public-read"
+
+  lifecycle_rule {
+    id      = "cleanup"
+    enabled = true
+    expiration {
+      days = 1
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_object" "html" {
+  key    = "index.html"
+  bucket = aws_s3_bucket.html.id
+  source = "index.html"
+  acl    = "public-read"
+}
