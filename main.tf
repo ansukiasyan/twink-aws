6b8968d33e943dc34d0842a03394708fc85354fb
ansukiasyan <an.sukiasyan@gmail.com>
2020-04-25 20:51:56 +0400

    Configure ssh for the instance, output instance public ip

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "amazon_linux" {
   ami                    = "ami-0323c3dd2da7fb37d"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.amazon_linux.id]
+  key_name               = aws_key_pair.annas.key_name
 
   user_data = file("cloud-config/get_script.sh")
 
@@ -14,6 +15,12 @@ resource "aws_instance" "amazon_linux" {
     Name = "web-server"
   }
   iam_instance_profile = aws_iam_instance_profile.s3_read.id
+}
+
+#add ssh key
+resource "aws_key_pair" "annas" {
+  key_name   = "annas"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDrrXHX5hDyxdfUyoW3ReZg9P7UwEwZfrRBlqM/3UK34cJxKzBLjfKWvP8c43urx+L6igP4yAz+EbCWzHXVqS94EmdjiXLkvdT+9/Zn+ZaiAe2lFXQ6H2dqkgq1/ZeC4Qlb6CoCFSQYgcfp2yTFM8drUStQpWvRupOE+STdaRtxvlBynno2QlKr7DbTFDS9L3ylvPzCRtiXrCjIPRerhy1GhZE5Vb0f76Z5ZV7yDxNvti2XUBir21uP2lt2mQBSvWDK9GZaxQ5Z7xmvgeSfwaVk194GHtk8ZdECYX9jEx0GGqJXqhgEJFJ37ujLWP5Amb/C5UOEWhbVbOh1cTBdUFvz annas@AMYW-AnnaS"
 }
 
 #iam role to mark ec2 instance as trusted
